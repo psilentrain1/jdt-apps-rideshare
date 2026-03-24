5445213d1e1050db1d653bfa7ae6266dd53c2c1d
James Drake <info@jamesmdrakemedia.com>
2026-03-24 19:36:54 -0400

    made useGetDashData more efficient

--- src/hooks/useGetDashData.ts
+++ src/hooks/useGetDashData.ts
@@ -5,61 +5,49 @@ import type { Month, MonthlyTotals, Ride } from "../utils/types";
 
 export function useGetDashData() {
   const [rideList, setRideList] = useState<Ride[]>([]);
-  const [monthList, setMonthList] = useState<Month[]>([]);
-  const [totals, setTotals] = useState<MonthlyTotals>({});
 
   useEffect(() => {
     getAllRides().then(setRideList);
   }, []);
 
-  function totalizeRides() {
-    const totalList: MonthlyTotals = {};
-    const localMonthList: Month[] = [];
+  const totalList: MonthlyTotals = {};
+  const monthList: Month[] = [];
 
-    rideList.map((ride) => {
-      const month = Number(ride.start_time.substring(0, 7).replace("-", ""));
-      const monthLabel = getMonthLabel(month);
-      if (!Object.hasOwn(totalList, month)) {
-        totalList[month] = {
-          monthLabel: monthLabel,
-          uberFare: 0,
-          uberTips: 0,
-          lyftFare: 0,
-          lyftTips: 0,
-          totalFare: 0,
-          totalTips: 0,
-          total: 0,
-        };
-        localMonthList.push({
-          label: monthLabel,
-          value: Number(month),
-        });
-      }
+  rideList.forEach((ride) => {
+    const month = Number(ride.start_time.substring(0, 7).replace("-", ""));
+    const monthLabel = getMonthLabel(month);
+    if (!Object.hasOwn(totalList, month)) {
+      totalList[month] = {
+        monthLabel: monthLabel,
+        uberFare: 0,
+        uberTips: 0,
+        lyftFare: 0,
+        lyftTips: 0,
+        totalFare: 0,
+        totalTips: 0,
+        total: 0,
+      };
+      monthList.push({
+        label: monthLabel,
+        value: Number(month),
+      });
+    }
 
-      if (ride.service === "uber") {
-        totalList[month].uberFare += ride.fare + ride.fee;
-        totalList[month].uberTips += ride.tip;
-      } else if (ride.service === "lyft") {
-        totalList[month].lyftFare += ride.fare + ride.fee;
-        totalList[month].lyftTips += ride.tip;
-      }
-    });
+    if (ride.service === "uber") {
+      totalList[month].uberFare += ride.fare + ride.fee;
+      totalList[month].uberTips += ride.tip;
+    } else if (ride.service === "lyft") {
+      totalList[month].lyftFare += ride.fare + ride.fee;
+      totalList[month].lyftTips += ride.tip;
+    }
+  });
 
-    // Calculate totals per month
-    Object.keys(totalList).forEach((month) => {
-      const monthData = totalList[month];
-      monthData.totalFare = monthData.uberFare + monthData.lyftFare;
-      monthData.totalTips = monthData.uberTips + monthData.lyftTips;
-      monthData.total = monthData.totalFare + monthData.totalTips;
-    });
+  Object.keys(totalList).forEach((month) => {
+    const monthData = totalList[month];
+    monthData.totalFare = monthData.uberFare + monthData.lyftFare;
+    monthData.totalTips = monthData.uberTips + monthData.lyftTips;
+    monthData.total = monthData.totalFare + monthData.totalTips;
+  });
 
-    setTotals(totalList);
-    setMonthList(localMonthList);
-  }
-
-  useEffect(() => {
-    totalizeRides();
-  }, [rideList]);
-
-  return { monthList, totals };
+  return { monthList, totals: totalList };
 }
