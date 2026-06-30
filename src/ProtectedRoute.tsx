f798bc35f2f9711e702e36cf089453412cc8e18f
James Drake <info@jamesmdrakemedia.com>
2026-06-29 22:04:11 -0400

    Added bearer token tracking and debug logging

--- src/ProtectedRoute.tsx
+++ src/ProtectedRoute.tsx
@@ -3,7 +3,7 @@ import { authClient } from "./lib/auth-client";
 
 export default function ProtectedRoute() {
   const { data: session, isPending } = authClient.useSession();
-
+  console.log("ProtectedRoute()", session);
   if (isPending) return <></>;
 
   return session ? <Outlet /> : <Navigate to="/login" replace />;
--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -2,4 +2,16 @@ import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
   baseURL: `${import.meta.env.VITE_API_ENDPOINT}/auth`,
+  fetchOptions: {
+    onSuccess: (ctx) => {
+      const authToken = ctx.response.headers.get("set-auth-token");
+      if (authToken) {
+        localStorage.setItem("token", authToken);
+      }
+    },
+    auth: {
+      type: "Bearer",
+      token: () => localStorage.getItem("token") || "",
+    },
+  },
 });
